d7bee07effc2a5f98809460fbeb1299d9b1e5486
Gemoroyy1 <69436242+Gemoroyy1@users.noreply.github.com>
2023-09-28 00:27:11 +0300

    Update main.py
    Структурное обновление касательно использования .env в коде

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 from telegram.ext import *
 
+# Просто в самом начале все значения из .env здесь же возьмем и дальше будем использовать упрощенно без os.getenv()
 load_dotenv()
+token=os.getenv('TELEGRAM_TOKEN')
 
 async def start_command(update, context):
     # Отправляем приветственное сообщение пользователю
@@ -16,7 +18,7 @@ async def read_command(update, context):
     await update.message.reply_text('Это была команда /read')
 
 if __name__=='__main__':
-    application = Application.builder().token(os.getenv('TELEGRAM_TOKEN')).build()
+    application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start',start_command))
     application.add_handler(CommandHandler('upload', upload_command))
     application.add_handler(CommandHandler('read', read_command))
