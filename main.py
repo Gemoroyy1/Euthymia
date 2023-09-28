ea4ae041ad4bd4929badad90c441eabe1e6e9403
Gemoroyy1 <69436242+Gemoroyy1@users.noreply.github.com>
2023-09-28 13:28:11 +0300

    Update main.py
    Первое подключение к БД

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
 import os
+import psycopg2
+from psycopg2 import Error
 from dotenv import load_dotenv
 from telegram.ext import *
 
-# Просто в самом начале все значения из .env здесь же возьмем и дальше будем использовать упрощенно без os.getenv()
 load_dotenv()
-token=os.getenv('TELEGRAM_TOKEN')
+token=os.getenv('TOKEN')
+user=os.getenv('USER')
+password=os.getenv('PASSWORD')
+host=os.getenv('HOST')
+port=os.getenv('PORT')
+database=os.getenv('DATABASE')
+
+connection=0
+
+try:
+    # Подключение к существующей базе данных
+    connection = psycopg2.connect(dbname=database, user=user, password=password)
+
+    # Курсор для выполнения операций с базой данных
+    cursor = connection.cursor()
+    # Распечатать сведения о PostgreSQL
+    print("Информация о сервере PostgreSQL")
+    print(connection.get_dsn_parameters(), "\n")
+    # Выполнение SQL-запроса
+    cursor.execute("SELECT version();")
+    # Получить результат
+    record = cursor.fetchone()
+    print("Вы подключены к - ", record, "\n")
+
+    # Выполнение SQL-запроса
+    quest='''SELECT * FROM public.test_table
+                ORDER BY id ASC '''
+    cursor.execute(quest)
+    # Получить результат
+    record = cursor.fetchone()
+    print(record, "\n")
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
 
 async def start_command(update, context):
     # Отправляем приветственное сообщение пользователю
